9c94315821f6eea255b27b69c0da8678d6022d4b
Jose Arias <josee.ariasmelo@gmail.com>
2023-09-19 17:46:04 -0300

    Add new consumer and update groupId value.

--- WeatherProducer/WeatherConsumer/Program.cs
+++ WeatherProducer/WeatherConsumer/Program.cs
@@ -3,33 +3,33 @@ using Newtonsoft.Json;
 
 var config = new ConsumerConfig
 {
-    GroupId = "weather-consumer-groupA",
+    GroupId = "weather-consumer-groupB",
     BootstrapServers = "localhost:9092",
     AutoOffsetReset = AutoOffsetReset.Earliest
 };
 
-using var consumer = new ConsumerBuilder<Null,string>(config).Build();
+using var consumer = new ConsumerBuilder<Null, string>(config).Build();
 
 consumer.Subscribe("weather-topic");
 
-CancellationTokenSource token = new ();
+CancellationTokenSource token = new();
 
 try
 {
-    while(true)
+    while (true)
     {
         var response = consumer.Consume(token.Token);
         if (response.Message != null)
         {
             var weather = JsonConvert.DeserializeObject<Weather>(response.Message.Value);
-            Console.WriteLine($"State: { weather.state}, Temp: {weather.temperature}F");
+            Console.WriteLine($"State: {weather.state}, Temp: {weather.temperature}F");
         }
     }
 }
 catch (Exception)
 {
 
-	throw;
+    throw;
 }
 
 public record Weather(string state, int temperature);
